c6c0e2b21e26ff93d4d56ebcb6151f2caed2c46a
Gustavo de los Campos <gdeloscampos@gmail.com>
2016-06-09 10:19:14 -0400

    Update BGLR2.R

--- R/BGLR2.R
+++ R/BGLR2.R
@@ -220,7 +220,7 @@ BGLR2=function (y, response_type = "gaussian", a = NULL, b = NULL,
     	 rm(nIter_call,burnIn_call,thin_call,saveAt_call)
     	
     	# Restore seed
-    	if(restoreSeed){ .Random.seed=seed }
+    	#if(restoreSeed){ .Random.seed=seed }
     	
     	# Reseting Running Means
     	if(resetRunningMeans){
