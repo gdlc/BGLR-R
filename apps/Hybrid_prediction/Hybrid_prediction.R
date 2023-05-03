3e5b719a39bf331f8c0b9baacc33ea8c77d02169
perpdgo <perpdgo@gmail.com>
2023-05-02 21:46:20 -0600

    Add files via upload

--- apps/Hybrid_prediction/Hybrid_prediction.R
+++ apps/Hybrid_prediction/Hybrid_prediction.R
@@ -5,9 +5,10 @@ RKHS.Hybrid_prediction<-function(y, location, id1, id2, idH,
                                  nIter=10000,burnIn=5000,thin=10,
                                  verbose=FALSE)
 {
+
+
 	location<-as.character(location)
 	location<-factor(location)
-	
 	ZE<-model.matrix(~location-1)
 	
 	id1<-as.character(id1)
@@ -22,17 +23,53 @@ RKHS.Hybrid_prediction<-function(y, location, id1, id2, idH,
 	idH<-factor(idH,levels=rownames(H))
 	Z3<-model.matrix(~idH-1)
 
-	G1star<-Z1%*%G1%*%t(Z1)
-	G2star<-Z2%*%G2%*%t(Z2)
-	Hstar<-Z3%*%H%*%t(Z3)
+	eigen_G1<-eigen(G1,symmetric=TRUE)
+	index<-eigen_G1$values>1e-10
+	eigen_G1$vectors<-eigen_G1$vectors[,index]
+	eigen_G1$values<-eigen_G1$values[index]
 
-	ETA<-list(list(X=ZE,model="FIXED"),
-	     list(K=G2star,model="RKHS"),
-         list(K=G2star,model="RKHS"),
-         list(K=Hstar,model="RKHS"))
 
+	eigen_G2<-eigen(G2,symmetric=TRUE)
+	index<-eigen_G2$values>1e-10
+	eigen_G2$vectors<-eigen_G2$vectors[,index]
+	eigen_G2$values<-eigen_G2$values[index]
+
+	eigen_H<-eigen(H,symmetric=TRUE)
+	index<-eigen_H$values>1e-10
+	eigen_H$vectors<-eigen_H$vectors[,index]
+	eigen_H$values<-eigen_H$values[index]
+
+
+	Z1star<-Z1%*%eigen_G1$vectors%*%sqrt(diag(eigen_G1$values))
+	Z2star<-Z2%*%eigen_G2$vectors%*%sqrt(diag(eigen_G2$values))
+	Z3star<-Z3%*%eigen_H$vectors%*%sqrt(diag(eigen_H$values))
+
+	#Linear predictor
+	ETA<-list(list(X=ZE,model="BRR"),
+              list(X=Z1star,model="BRR"),
+       	      list(X=Z2star,model="BRR"),
+              list(X=Z3star,model="BRR"))
+           
 	fm<-BGLR(y=y,ETA=ETA,nIter=nIter,burnIn = burnIn,thin=thin,
         verbose=verbose)
 
+	#Getting the mean of random effects and add the results to the output	
+	u1<-as.vector(eigen_G1$vectors%*%sqrt(diag(eigen_G1$values))%*%fm$ETA[[2]]$b)
+	names(u1)<-rownames(G1)
+	fm$ETA[[2]]$u<-u1
+
+	u2<-as.vector(eigen_G2$vectors%*%sqrt(diag(eigen_G2$values))%*%fm$ETA[[3]]$b)
+	names(u2)<-rownames(G2)
+	fm$ETA[[3]]$u<-u2
+
+	h<-as.vector(eigen_H$vectors%*%sqrt(diag(eigen_H$values))%*%fm$ETA[[4]]$b)
+	names(h)<-rownames(H)
+	fm$ETA[[4]]$u<-h
+
+	return(fm)
+	
 }
+
+
+
 
