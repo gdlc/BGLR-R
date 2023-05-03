95296baa16690172e356d791161bca1d8f255517
perpdgo <perpdgo@gmail.com>
2023-05-02 21:52:22 -0600

    Update Hybrid_prediction.R

--- apps/Hybrid_prediction/Hybrid_prediction.R
+++ apps/Hybrid_prediction/Hybrid_prediction.R
@@ -1,6 +1,6 @@
 
 
-RKHS.Hybrid_prediction<-function(y, location, id1, id2, idH,
+BRR.Hybrid_prediction<-function(y, location, id1, id2, idH,
 								 G1, G2, H, 
                                  nIter=10000,burnIn=5000,thin=10,
                                  verbose=FALSE)
