84d8ec8237b6ecac9b538f12de6c7d8cc622fb9b
Gustavo de los Campos <gdeloscampos@gmail.com>
2023-10-03 10:04:37 -0400

    Update mixturesWithNonZeroPriorMeans.R

--- misc/mixturesWithNonZeroPriorMeans.R
+++ misc/mixturesWithNonZeroPriorMeans.R
@@ -73,7 +73,7 @@ MMS=function(C,rhs,my,vy,B0,nIter=150,burnIn=50,R2=.5,nComp=matrix(ncol(B0)),
 
 if(FALSE){
 
- rm(list=ls())
+  rm(list=ls())
 
  library(BGLR)
  data(wheat)
@@ -101,108 +101,14 @@ if(FALSE){
 
  B0=cbind(rep(0,p),-1,1)
  
- tmp=MMS(C=C,rhs=rhs,my=mean(y),vy=var(y),B0=cbind(rep(0,p),-1,1))
- plot(tmp$POST.PROB[,1],col=colQTL,pch=ifelse(colQTL==8,1,19),ylim=c(0,1))
+ source('https://raw.githubusercontent.com/gdlc/BGLR-R/master/misc/mixturesWithNonZeroPriorMeans.R')
+ tmp=MMS(C=C,rhs=rhs,my=mean(y),vy=var(y),B0=cbind(rep(0,p),-1,1),nIter=1500,burnIn=500)
+ 
+ 
+ 
+  plot(tmp$POST.PROB[,1],col=colQTL,pch=ifelse(colQTL==8,1,19),ylim=c(0,1))
   plot(tmp$POST.PROB[,2],col=colQTL,pch=ifelse(colQTL==8,1,19),ylim=c(0,1))
   plot(tmp$POST.PROB[,3],col=colQTL,pch=ifelse(colQTL==8,1,19),ylim=c(0,1))
 
 }
 
-
-## Old code
-if(FALSE){
-rm(list=ls())
-
-library(BGLR)
-data(wheat)
-
-X=scale(wheat.X,center=TRUE,scale=FALSE)
-
-X=X[,1:100]
-
-p=ncol(X)
-n=nrow(X)
-
-QTL=seq(from=5,to=95,by=10)
-b0=rep(0,p)
-b0[QTL]=rep(c(-1,1),each=5)
-
-colQTL=ifelse(b0==0,8,ifelse(b0==1,2,4))
-signal=X%*%b0
-error=rnorm(n=n,sd=sd(signal))
-y=signal+error
-
-varE=var(error)
-
-C=crossprod(X)
-rhs=crossprod(X,y)
-
-priors=data.frame(mu=c(0,-1,1),var=rep(.1,3))
-
-
-nIter=100
-
-b=rep(0,p)
-d=rep(1,p) # indicator variable for the group
-
-POST.PROB=matrix(nrow=p,ncol=nrow(priors),0)
-
-S0.b=.05
-df0.b=5
-
-postMeanB=rep(0,p)
-piros$postMeanVar=0
-
-PROBS=matrix(nrow=p,ncol=nrow(priors))
- 	
- 	
-for(i in 1:nIter){
-
- for(j in 1:p){
- 	
- 	offset=sum(C[j,-j]*b[-j])
- 	rhs_j=(rhs[j]-offset)/varE
- 	rhs_j=rhs_j+priors$mu[d[j]]/priors$var[d[j]]
- 	
- 	
- 	lhs_j=C[j,j]/varE+1/priors$var[d[j]]
- 	
- 	sol=rhs_j/lhs_j
- 	b[j]=rnorm(n=1,mean=sol,sd=sqrt(1/lhs_j))
- 	
-
-
- }
- postMeanB=postMeanB+b/nIter
- 
- ## Sampling mixture components 
- for(k in 1:ncol(PROBS)){
- 	 PROBS[,k]=dnorm(b,mean=priors$mu[k],sd=sqrt(priors$var[k]))	
- }
- d=apply(FUN=sample,x=1:ncol(PROBS),X=PROBS,size=1,MARGIN=1,replace=TRUE)
-
- ## Sampling the variance of the mixture components
- for(k in 1:ncol(PROBS)){
-     tmp=(d==k)
-     
-     DF=sum(tmp)
-     SS=S0.b
-     if(DF>0){
-	 	 SS=SS+sum(b[tmp]^2)
- 	 }
- 	 priors$var[k]=SS/rchisq(df=DF,n=1) 
- 	 priors$postMeanVar[k]= priors$postMeanVar[k]+priors$var[k]/nIter
- }
- 
- ## computing posterior mean of mixture probabilities
- for(k in 1:ncol(POST.PROB)){
-     tmp=(d==k)
-     
- 	 POST.PROB[tmp,k]=POST.PROB[tmp,k]+1/nIter
-  }
-
- 
-}
-
-return(list(b=postMeanB,POST.PROB=POST.PROB,postMeanVarB=postMeanVarB))
-}
