827ccf007993af22ce056aa99f90371d66c744e1
Gustavo de los Campos <gdeloscampos@gmail.com>
2025-03-12 18:07:21 -0300

    Update getInt.R

--- apps/INT/getInt.R
+++ apps/INT/getInt.R
@@ -1,4 +1,5 @@
 getInt=function(X,eID,gID=rep(1:nrow(X)),sparse=TRUE,centerX=FALSE,...){
+	stopifnot(all(GID%in%rownames(X)))
 	ETA=list()
 	X=X[gID,]
 	if(centerX){
@@ -20,5 +21,8 @@ getInt=function(X,eID,gID=rep(1:nrow(X)),sparse=TRUE,centerX=FALSE,...){
 		}
 	}
 	names(ETA)=c('main',paste0('int_',levels))
+
+	W=as.matrix(model.matrix(~factor(eID))[,-1]
+        ETA=c(list(X=W,model='FIXED'),ETA)
 	return(ETA)
 }
